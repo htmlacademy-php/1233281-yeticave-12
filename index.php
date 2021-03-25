f142add4604f57c64c74b2230fe7be4cad52c48f
Alexandr <timoshenskiy.ad@gmail.com>
2021-03-25 09:24:31 +0300

    add camelCase + fix operator

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$is_auth = rand(0, 1);
+$isAuth = rand(0, 1);
 
-$user_name = 'Alexandr';
+$userName = 'Alexandr';
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -29,9 +29,9 @@ $user_name = 'Alexandr';
                 <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
                 <nav class="user-menu">
-                    <?php if ($is_auth == 1) : ?>
+                    <?php if ($isAuth === 1) : ?>
                         <div class="user-menu__logged">
-                            <p><?= $user_name; ?></p>
+                            <p><?= $userName; ?></p>
                             <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                             <a class="user-menu__logout" href="#">Выход</a>
                         </div>
